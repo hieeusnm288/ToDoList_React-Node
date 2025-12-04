ea58a0765370786833459fa82bb4985ccc779656
hieunm <hieunm@mail.wgentech.com>
2025-12-04 13:22:42 +0700

    Update for deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,25 +3,37 @@ import taskRoutes from './routes/taskRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 const app = express();
 
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }
-));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors(
+    {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true
+    }
+  ));
+}
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
+if (process.env.NODE_ENV = 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend", 'dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", 'dist', 'index.html'));
+  });
+}
+
 connectDB().then(() => {
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
 
 
